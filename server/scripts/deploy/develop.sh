6e6202a0cd67f8d4226cf9d058fba48545de3abe
Ian Chapman <72579524+LazyGuyWithRSI@users.noreply.github.com>
2024-10-18 15:43:07 -0700

    Change repo names

--- server/scripts/deploy/develop.sh
+++ server/scripts/deploy/develop.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-curl -o docker-compose.yml https://raw.githubusercontent.com/refracta/dcss-server/develop/server/docker-compose.yml && \
-curl -o docker-compose.ports.yml https://raw.githubusercontent.com/refracta/dcss-server/develop/server/docker-compose.ports.yml && \
+curl -o docker-compose.yml https://raw.githubusercontent.com/LazyGuyWithRSI/dcss-server/develop/server/docker-compose.yml && \
+curl -o docker-compose.ports.yml https://raw.githubusercontent.com/LazyGuyWithRSI/dcss-server/develop/server/docker-compose.ports.yml && \
 docker compose -f docker-compose.yml -f docker-compose.ports.yml config > docker-compose.combine.yml && \
 mv docker-compose.combine.yml docker-compose.yml && \
 rm docker-compose.ports.yml && \
-docker compose down && docker rmi refracta/dcss-server || true && \
+docker compose down && docker rmi LazyGuyWithRSI/dcss-server || true && \
 docker compose run --rm -T -e CMD='cd $DGL_CONF_HOME && git pull' dcss-server && \
 docker compose run --rm -T -e CMD='$SCRIPTS/utils/release.sh download -o -p /data -n game-data' dcss-server && \
 docker compose up -d && docker compose logs -f
